b3b12b39e2b4bd67b9a9906035cc9c4170a7be2a
Alejandro <alejandrocondori0507@gmail.com>
2025-02-06 20:05:59 -0500

    database

--- appcontrol/settings.py
+++ appcontrol/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,11 +81,9 @@ WSGI_APPLICATION = 'appcontrol.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default="postgresql://postgres:IppBwuQmOpZRmfhXYsDmAZqpzooazusA@postgres.railway.internal:5432/railway")
 }
 
 
