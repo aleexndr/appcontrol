2c1fd6fa24d37d9ce83a5eaa0d989374eaa10341
Alejandro <alejandrocondori0507@gmail.com>
2025-02-10 12:55:17 -0500

    alertas

--- registro/views.py
+++ registro/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Alimentos
 
 # Create your views here.
@@ -13,9 +14,18 @@ def registrar_alimentos(request):
         cantidad = request.POST.get('cantidad')
         costo = request.POST.get('costo')
 
+        if not cantidad.isdigit():
+            messages.error(request, "Cantidad invalida")
+            return redirect('index')
+    
+        # if not costo.isdigit():
+        #     messages.error(request, "Costo invalido")
+        #     return redirect('index')
+
         if nombre and tipo and cantidad and costo:
             alimentos = Alimentos(nombre=nombre, tipo=tipo, cantidad=cantidad, costo=costo)
             alimentos.save()
+            messages.success(request, "Guardado")
             return redirect('index')
         
     return render(request, 'index.html')
